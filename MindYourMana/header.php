849478ee45565aa8aba2eebfd016caefa826c1a5
Nathan Baines <nathan.baines@nathcat.net>
2025-12-07 15:15:05 +0000

    Actually fixed page name

--- MindYourMana/header.php
+++ MindYourMana/header.php
@@ -1,7 +1,7 @@
 <div class="navbar column align-center justify-center">
     <div style="width: 100%" class="row align-center justify-center">
         <div class="column align-center justify-center">
-            <h1 id="navbar-title">MindOverMana</h1>
+            <h1 id="navbar-title">Mind Your Mana</h1>
             <a href="/">Part of the Nathcat Network</a>
         </div>
     </div>
